6ec0a6aef7b74ea9c0eb796f431cf3db8c5a1a3b
limminjeong98 <limminjeong98@naver.com>
2024-08-29 17:16:54 +0900

    Order 도메인 구현

--- src/main/java/com/example/order/common/exception/IllegalStatusException.java
+++ src/main/java/com/example/order/common/exception/IllegalStatusException.java
@@ -2,12 +2,12 @@ package com.example.order.common.exception;
 
 import com.example.order.common.response.ErrorCode;
 
-public class IllegalStateException extends BaseException {
-    public IllegalStateException() {
+public class IllegalStatusException extends BaseException {
+    public IllegalStatusException() {
         super(ErrorCode.COMMON_ILLEGAL_STATUS);
     }
 
-    public IllegalStateException(String message) {
+    public IllegalStatusException(String message) {
         super(message, ErrorCode.COMMON_ILLEGAL_STATUS);
     }
 }
